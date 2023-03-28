daebf195384ca9fb56e12e188a584e6a6d4c7249
janardhan <janardhanmunugal@gmail.com>
2023-03-28 16:25:29 +0530

    about title change

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -19,7 +19,7 @@ function template() {
             <div className="rigt">
               <h2>About Us</h2>
               <h4>To Grow Your Business</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              <p> <b>About Project:</b> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
               <a herf="">Read More</a>
